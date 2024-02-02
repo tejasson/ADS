817e1cbcdef668c1847d0921097b59ceb75ca183
pappuGhisela <144206036+tejasson@users.noreply.github.com>
2024-02-02 12:05:12 +0530

    Update ass2-GraphTraversal.cpp

--- ass2-GraphTraversal.cpp
+++ ass2-GraphTraversal.cpp
@@ -1,60 +1,69 @@
-// Graph traversal: The area around the college and the prominent landmarks of it are represented using graphs. Write a menu driven C++ program to represent this as a graph using adjacency matrix /list and perform DFS and BFS.
+// Graph traversal: The area around the college and the prominent
+// landmarksNode of it are represented using graphs.
+// Write a menu driven C++ program to represent this as a graph using
+// adjacency matrix /list and perform DFS and BFS.
+
+// https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 
 #include<iostream>
+#include<stack>
 using namespace std;
 
-class Vertices {
+class landmarksNode {
     public:
         string place;
-        Vertices *next;
+        landmarksNode *next;
 };
 
 class GraphTraversal {
-    Vertices *places[100];
-    int placesIndex, flag;
-    Vertices *firstplace = new Vertices;
-    Vertices *nextPlace = new Vertices;
-    Vertices *temp;
-    
-    public:
-        GraphTraversal() {
-            placesIndex = 0;
-        }
-        void addEdge(string place1, string place2) {
-            //to check if the place1 already is in the array or not
-            for (int i = 0; i < placesIndex; i++) {
-                flag = 0;
-                if (places[i]->place == place1) {
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag != 1) {
-                places[placesIndex] = firstplace;
-                firstplace->place = place1;
-                firstplace->next = NULL;
-                placesIndex++;
-            }        
+    int landmarkIndex = 0;
+    landmarksNode *newPlace = new landmarksNode;
+    landmarksNode *temp;
 
-            //to add edge
-            for (int i = 0; i < placesIndex; i++) {
-                temp = places[i];
-                if (temp->place == place1) {
-                    while (temp->next != NULL) {
+    landmarksNode *landmarksArr[10];
+
+    string visited[10];
+
+    public:
+        void addplace (string place) {
+            landmarksArr[landmarkIndex] = newPlace;
+            newPlace->place = place;
+            newPlace->next = NULL;
+            landmarkIndex++;
+
+            cout<<"landmark added"<<endl;
+        }
+
+        void addedge (string from, string to) {
+            for (int i = 0; i < landmarkIndex; i++) {
+                if (from == landmarksArr[i]->place) {
+                    temp = landmarksArr[i];
+                    while (temp != NULL) {
                         temp = temp->next;
                     }
-                    temp->next = nextPlace;
-                    nextPlace->place = place2;
-                    nextPlace->next = NULL;
+                    temp->next = newPlace;
+                    newPlace->place = to;
+                    newPlace->next = NULL;
                 }
             }
-            cout<<"added"<<endl;
+            cout<<"edge added"<<endl;
+        }
+
+        void BFSTraversal () {
+
         }
 };
 
 int main() {
     GraphTraversal obj;
-    obj.addEdge("library","xerox");
+    obj.addplace("engg");
+    obj.addplace("poly");
+    obj.addplace("pharma");
+    obj.addplace("canteen");
+    obj.addplace("parking");
+
+    obj.addedge("engg","parking");
+    obj.addedge("engg","canteen");
 
     return 0;
 }
